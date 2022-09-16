d6826bc21bc10870a3eea72f3aeb967d3021fc06
AryanB-23bit <50510280+AryanB-23bit@users.noreply.github.com>
2022-09-16 16:49:04 -0400

    Aryan feats (#3)
    * Merge conflict changes
    
    * Adding hot keys
    
    * merge conflcits resolved
    
    * Adding hot keys and time frames. Switched the markers
    
    Co-authored-by: aryanbisht <aryan.bisht@thedeveffect.com>

--- src/App.js
+++ src/App.js
@@ -15,6 +15,33 @@ function App() {
   let currTime = null;
   const [listOfStartStop, setListOfStartStop] = useState([]);
   const increment = 50/videoDuration;
+  const vidTimeControls = 5;
+  const [recordState, setRecordState] = useState(false)
+
+  const HandleKeyUp = (event) => {
+    if (event.key.toLowerCase() === "s"){
+      getStopTimeStamp()
+      setRecordState(false)
+    }
+  }
+
+  const HandleKeyDown = (event) => {
+    switch (event.key.toLowerCase()){
+      case "s":
+        getStartTimeStamp()
+        setRecordState(true)
+        break;
+      case "j":
+        video.current.currentTime -= vidTimeControls
+        break;
+      case "k":
+        video.current.paused ? video.current.play() : video.current.pause();
+        break;
+      case "l":
+        video.current.currentTime += vidTimeControls
+        break;
+    }
+  }
 
   useEffect(() => {
     if (video.current) {
@@ -25,15 +52,12 @@ function App() {
   function getStartTimeStamp(){
     if(!!video){
       setStartPos(video.current.currentTime*increment);
-      console.log("Start Pos Set: "+video.current.currentTime);
-
     }
   }
 
   function getStopTimeStamp(){
     if(!!video){
       setStopPos(video.current.currentTime*increment);
-      console.log("Stop pos set: "+video.current.currentTime);
       const posPair = {
         "startPos":startPos,
         "stopPos":video.current.currentTime*increment
@@ -65,7 +89,7 @@ function App() {
   }
 
   function onPlay(){
-    if (video.current.paused == true) {
+    if (video.current.paused === true) {
       video.current.play();
     } else {
       video.current.pause();
@@ -90,14 +114,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Video App</p>
-          <video controls width="1000" ref={video} onTimeUpdate={() => {
+          <video controls width="1000" ref={video} onKeyDown={HandleKeyDown} onKeyUp={HandleKeyUp} onTimeUpdate={() => {
           calcTime();
           loop();
         }}>
-          <source src="flower.webm" type="video/webm"></source>
+          <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" type="video/mp4"></source>
           </video>
-
           <div id="video-controls">
             <button type="button" id="play-pause" onClick={() => {
             handleTimer();
@@ -108,9 +130,15 @@ function App() {
           <button id="startTimeButton" onClick={getStartTimeStamp}>Set Start Timestamp</button>
           <button id="stopTimeButton" onClick={getStopTimeStamp}>Set Stop Timestamp</button>
           <button id="clearLoopButton" onClick={clearTime}>Clear Loop</button>
-          
+
+        { recordState === true &&
+            <div>
+              <h1>🔴 Time Frame Recording!</h1>
+            </div>
+        }
+          <p>Scrub forward +5 second with "L" key and back -5 seconds with "J" key. Play/Pause with "K" key.</p>
       </header>
     </div>
   );
-};
+}
 export default App;
