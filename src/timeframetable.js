2aae4c2fbfd153c06d75e2f43c0f179892c25a66
aryanbisht <aryan.bisht@thedeveffect.com>
2022-09-29 09:40:39 -0400

    function that adds timeframes to scrubbar, slider.js and timeframetable.js

---
+++ src/timeframetable.js
@@ -0,0 +1,31 @@
+// import React from "react";
+// import {Marker} from "./marker";
+//
+// const Rows = ({timeFrame}) => {
+//     return (
+//         timeFrame.map((pos, index) => {
+//                 return (
+//                     <Marker
+//                         key={index}
+//                         segmentIndex={index}
+//                         start={pos.getStartTimePos()}
+//                         stop={pos.getStopTimePos()}
+//                         onClick={eventspot}
+//                         onTimeUpdate={onTimeUpdate}
+//                     />
+//         <tr>
+//             <td>{timeFrame.getStartTimePos()}</td>
+//             <td>{timeFrame.getStopTimePos()}</td>
+//             <td>
+//                 <button>
+//                     Edit
+//                 </button>
+//                 <button>
+//                     Delete
+//                 </button>
+//             </td>
+//         </tr>
+//     );
+// };
+//
+// export default Rows;
